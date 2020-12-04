e9bb513d20977a239264972c98aca11f501ebf78
Artur Campos <arturcamposba@gmail.com>
2020-12-04 16:44:08 -0300

    Resposta exercicio 1

--- 01_donuts.py
+++ 01_donuts.py
@@ -1,20 +1,20 @@
 """
-01. donuts
-
-Dado um contador inteiro do numero de donuts, retorne uma string
-com o formato 'Number of donuts: <count>' onde <count> é o numero
-recebido. Entretanto, se o contador for 10 ou mais, use a palavra 'many'
-ao invés do contador.
-Exemplo: donuts(5) retorna 'Number of donuts: 5'
-e donuts(23) retorna 'Number of donuts: many'
+01. Donuts
+Dado um contador inteiro do número de donuts, retorne uma string com o formato 'Number of donuts: <count>' onde <count>
+é o número recebido. Entretanto, se o contador for 10 ou mais, use a palavra 'many' ao invés do contador.
+Exemplo: donuts(S) retorna 'Number of donuts: 5' e donuts(23) retorna 'Number of donuts: many'.
 """
 
-def donuts(count):
-    # +++ SUA SOLUÇÃO +++
+def count_donuts(count):
+    #minha solução!
+    if count > 10:
+        print(f'Donuts({count}). Number of Donuts: many')
+    else:
+        print(f'Number of Donuts: {count}')
+
     return
 
 
-# --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
 
 def test(f, in_, expected):
     """
@@ -32,10 +32,9 @@ def test(f, in_, expected):
 
     print(f'{sign} {f.__name__}({in_!r}) retornou {out!r} {info}')
 
-
+# Teste
 if __name__ == '__main__':
-    # Testes que verificam o resultado do seu código em alguns cenários.
-    test(donuts, 4, 'Number of donuts: 4')
-    test(donuts, 9, 'Number of donuts: 9')
-    test(donuts, 10, 'Number of donuts: many')
-    test(donuts, 99, 'Number of donuts: many')
+    count_donuts(4)
+    count_donuts(9)
+    count_donuts(10)
+    count_donuts(99)
